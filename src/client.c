2ff635c72b0bc84a03e41b2411cdf93cf97ea6e5
ilyan <ilyanbendib@gmail.com>
2024-01-15 17:03:12 +0100

    s

--- src/client.c
+++ src/client.c
@@ -6,19 +6,24 @@
 /*   By: ilbendib <ilbendib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 12:53:06 by ilbendib          #+#    #+#             */
-/*   Updated: 2024/01/15 15:08:38 by ilbendib         ###   ########.fr       */
+/*   Updated: 2024/01/15 16:56:19 by ilbendib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
-// void ft_check_output(int signal)
-// {
-// 	if (signal == SIGUSR1)
-// 	{
-		
-// 	}
-// }
+int validation_check;
+
+void	ft_check_output(int signal)
+{
+	if (signal == SIGUSR2)
+	{
+		ft_printf("Message sent and received.\n");
+		exit(EXIT_SUCCESS);
+	}
+	else if (signal == SIGUSR1)
+		validation_check = 1;
+}
 void	ft_check_bit(int pid, char i)
 {
 	int bit;
@@ -26,12 +31,14 @@ void	ft_check_bit(int pid, char i)
 	bit = 0;
 	while (bit < 8)
 	{
-		if ((i & (0x01 << bit)) != 0)
-			kill (pid, SIGUSR1); 
+		validation_check = 0;
+		if (i & (0x01 << bit))
+			kill (pid, SIGUSR1);
 		else
 			kill (pid, SIGUSR2);
-		usleep(250);
 		bit++;
+		while(validation_check != 1)
+			usleep(100);
 	}
 }
 
@@ -42,15 +49,16 @@ int	main(int argc, char **argv)
 
 	pid = ft_atoi(argv[1]);
 	i = -1;
-	// signal(SIGUSR1, ft_check_output);
-	// signal(SIGUSR2, ft_check_output);
+	signal(SIGUSR1, &ft_check_output);
+	signal(SIGUSR2, &ft_check_output);
 	if (argc == 3)
 	{
 		while (argv[2][++i])
 			ft_check_bit(pid, argv[2][i]);
 		ft_check_bit(pid, '\n');
 		ft_check_bit(pid, 0);
-		// while (1)
-		// 	pause();
+		while (1)
+			pause();
 	}
 }
+
--- src/serveur.c
+++ src/serveur.c
@@ -6,7 +6,7 @@
 /*   By: ilbendib <ilbendib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 13:12:38 by ilbendib          #+#    #+#             */
-/*   Updated: 2024/01/15 15:01:59 by ilbendib         ###   ########.fr       */
+/*   Updated: 2024/01/15 16:57:10 by ilbendib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,21 @@ void ft_check_signal(int signal, siginfo_t *info, void *note_use)
 	static int bit;
 	static int i;
 
-	(void)info;
 	(void)note_use;
 	if (signal == SIGUSR1)
 	{
 		i |= (0x01 << bit);
 	}
-	bit++;
-
 	if (bit == 8)
 	{
-		printf("%c", i);
+		if (i == 0)
+			kill (info->si_pid, SIGUSR2);
+		ft_printf("%c", i);
 		bit = 0;
 		i = 0;
 	}
+	kill(info->si_pid, SIGUSR1);
+	bit++;
 }
 
 int	main(void)
@@ -49,7 +50,7 @@ int	main(void)
 	action.sa_sigaction = ft_check_signal;
 	sigaction(SIGUSR1, &action, NULL);
 	sigaction(SIGUSR2, &action, NULL);
-	printf("PID server -> %d\n", pid);
+	ft_printf("PID server -> %d\n", pid);
 	while (1)
 		pause();
 	return (0);
